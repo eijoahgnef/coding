ec25ed71899d1ad3e308b072d7636b748b194c1b
冯浩洁 <1686913847@qq.com>
2023-02-28 20:51:54 +0800

    变量提升

---
+++ JavaScript/预编译/1.js
@@ -0,0 +1,9 @@
+console.log(x);  //  undefiend
+console.log(xFn);  //  [Functtion: xFn]
+var x  = 2
+function xFn() {}
+
+console.log(y)  // undefined
+console.log(yFn);   //   undefined
+var y  = 3 
+var yFn = () => {}
