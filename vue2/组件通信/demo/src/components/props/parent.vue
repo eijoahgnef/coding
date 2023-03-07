d96a30ecc427391e9ae9d6d07f6de6eb6675a1e5
冯浩洁 <1686913847@qq.com>
2023-03-07 15:18:56 +0800

    props

---
+++ vue2/组件通信/demo/src/components/props/parent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="parent">
+    <div class="wrap">
+        <span><input type="text" v-model="msg"></span>
+        <button @click="sub">确定</button>
+    </div>
+    <son :list="list"></son>
+  </div>
+</template>
+
+<script>
+import son from './son.vue';
+export default {
+    components: {
+        son
+    },
+    data() {
+        return {
+            msg: '',
+            list: []
+        }
+    },
+    methods: {
+        sub() {
+            this.list.push(this.msg)
+            this.msg = ''
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/props/son.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="son">
+    <ul>
+        <li v-for="(item, index) in msgList" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        list: Array
+    },
+    data() {
+        return {
+            msgList: this.list
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
