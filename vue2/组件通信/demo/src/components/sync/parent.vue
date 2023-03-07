a10d1aee32c186dac54ff84dfbb5bf01c87656a0
冯浩洁 <1686913847@qq.com>
2023-03-07 16:03:53 +0800

    sync

---
+++ vue2/组件通信/demo/src/components/sync/parent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sync">
+    
+    <div class="wrap">
+        <span><input type="text" v-model="msg"></span>
+        <button @click="sub">确定</button>
+    </div>
+    <!-- 双向绑定  :variable.sync="variable" -->
+    <son :list.sync="list"></son>
+  </div>
+</template>
+
+<script>
+import son from '../sync/son.vue'
+export default {
+    components: {
+        son
+    },
+    data() {
+        return {
+            msg: '',
+            list: []
+        }
+    },
+    methods: {
+        sub() {
+            this.list.push(this.msg)
+            this.msg = ''
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/sync/son.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="son">
+    <ul>
+      <li v-for="(item, index) in msgList" :key="index">
+        <span>{{ item }}</span>
+        <span @click="cancel(index)">删除</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array,
+  },
+  data() {
+    return {
+      msgList: this.list,
+    };
+  },
+  methods: {
+    cancel(index) {
+        this.msgList.splice(index, 1)
+    }
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+span
+    margin 0 10px
+</style>>
