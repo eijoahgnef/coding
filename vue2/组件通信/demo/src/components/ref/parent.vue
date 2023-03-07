7b9d67df5a0dae38a0bca24362e8e7233d6583f9
冯浩洁 <1686913847@qq.com>
2023-03-07 23:51:00 +0800

    ref

---
+++ vue2/组件通信/demo/src/components/ref/parent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ref">
+    <son ref="child"></son>
+    <ul>
+        <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import son from './son.vue'
+export default {
+    data() {
+        return {
+            list: []
+        }
+    },
+    components: {
+        son
+    },
+    mounted() {
+        this.list = this.$refs.child?.list
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/ref/son.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="son">
+    <span><input type="text" v-model="msg"></span>
+    <button @click="add">添加</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: '',
+            list: []
+        }
+    },
+
+    methods: {
+        add() {
+            if (!this.msg) return
+            this.list.push(this.msg)
+            console.log(this.list);
+            this.msg = ''
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
