874a8f4f7459d8e43ddce7b6010aad4922686651
冯浩洁 <1686913847@qq.com>
2023-03-07 21:27:50 +0800

    v-model

---
+++ vue2/组件通信/demo/src/components/model/parent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="model">
+    <son v-model="value"></son>
+    <div>{{ value }}</div>
+    <button @click="change">改变</button>
+    <!-- <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <span>{{ item }}</span>
+        <span @click="cancel(index)">删除</span>
+      </li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+import son from "./son.vue";
+export default {
+  components: {
+    son,
+  },
+  data() {
+    return {
+      value: 'haha',
+    };
+  },
+  methods: {
+    change() {
+        this.value = '我变了'
+    }
+  },
+};
+</script>
+
+<style></style>
---
+++ vue2/组件通信/demo/src/components/model/son.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="son">
+    
+    <div class="wrap">
+        <span><input type="text" :value="value" @input="handleChange"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["value"],
+  methods: {
+    handleChange(e) {
+        // console.log(e.target.value);
+        this.$emit("input", e.target.value)
+    }
+
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+span
+    margin 0 10px
+</style>>
