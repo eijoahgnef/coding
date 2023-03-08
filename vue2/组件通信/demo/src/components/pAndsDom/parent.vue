7b8764e6e37ab78d10d618de3707f52dd113073c
冯浩洁 <1686913847@qq.com>
2023-03-08 16:07:14 +0800

    $parent&$children

---
+++ vue2/组件通信/demo/src/components/pAndsDom/parent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="parent">
+    <div class="wrap">
+        <span>在父组件里父组件的信息：{{ info }}</span>
+        <span>在父组件里子组件的信息：{{ sonVal }}</span>
+    </div>
+    <button @click="changeSonInfo">改变子组件信息</button>
+    <son></son>
+  </div>
+</template>
+
+<script>
+import son from './son.vue'
+export default {
+    components: {
+        son
+    },
+    data() {
+        return {
+            info: '我是父组件',
+            sonVal: ''
+        }
+    },
+    mounted() {
+        this.sonVal = this.$children[0].info
+    },
+    methods: {
+        changeSonInfo() {
+            this.$children[0].changeSon()
+        },
+        changeInfo() {
+            this.info = '我变了'
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scope>
+.wrap
+    display flex
+    flex-direction column
+</style>
---
+++ vue2/组件通信/demo/src/components/pAndsDom/son.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="son">
+    <div class="wrap">
+        <span>在子组件里父组件的信息：{{ fatherVal }}</span>
+        <span>在子组件里子组件的信息： {{ info }}</span>
+    </div>
+    <button @click="changeFatherInfo">改变父组件信息</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            info: '我是子组件',
+            fatherVal: ''
+        }
+    },
+    mounted() {
+        this.fatherVal = this.$parent.info
+    },
+    methods: {
+        changeSon() {
+            this.info = '我变了'
+        },
+        changeFatherInfo() {
+            this.$parent.changeInfo()
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrap
+    display flex
+    flex-direction column
+</style>
