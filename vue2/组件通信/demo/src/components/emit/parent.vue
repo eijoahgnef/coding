578257adf93247407b429e197e5c2e156bacbc1a
冯浩洁 <1686913847@qq.com>
2023-03-08 10:44:35 +0800

    emit

---
+++ vue2/组件通信/demo/src/components/emit/parent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="emit">
+    <!-- <son v-on:emitList="gainList"></son> -->
+    <son @emitList="gainList"></son>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import son from './son.vue'
+export default {
+    components: {
+        son
+    },
+    data() {
+      return {
+        list:[]
+      }
+    },
+    methods: {
+      gainList(list) {
+        this.list = list
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/emit/son.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="son">
+    <span><input type="text" v-model="msg"></span>
+    <button @click="add">添加</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: '',
+            list: []
+        }
+    },
+    methods: {
+        add() {
+            if (!this.msg) return
+            this.list.push(this.msg)
+            this.msg = ''
+            this.$emit('emitList', this.list)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
