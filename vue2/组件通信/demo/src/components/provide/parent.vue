c6c204aa8d2457b43022a3007244e488b2a8f3ac
冯浩洁 <1686913847@qq.com>
2023-03-08 21:23:01 +0800

    provide&inject

---
+++ vue2/组件通信/demo/src/components/provide/parent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="parent">
+    <son></son>
+  </div>
+</template>
+
+<script>
+import son from './son.vue'
+export default {
+    components: {
+        son
+    },
+    provide: {
+        list: ['123','hahah']
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/provide/son.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="son">
+    <ul>
+        <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    inject: ['list'],
+    data() {
+        return {
+            
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
