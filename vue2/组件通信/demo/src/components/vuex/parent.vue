767f1be6943d50e2c124e6af5fe8b2e7aae00600
冯浩洁 <1686913847@qq.com>
2023-03-09 21:26:27 +0800

    vuex

---
+++ vue2/组件通信/demo/src/components/vuex/parent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="vuex">
+    <span>{{ getMsg }}</span>
+    <son></son>
+  </div>
+</template>
+
+<script>
+import son from './son.vue'
+import store from '../../store'
+export default {
+    components: {
+        son
+    },
+    computed: {
+        getMsg () {
+            return store.state.loginStatus
+        }
+    },
+    data() {
+        return {
+            // msg: ''
+        }
+    },
+    mounted() {
+        // this.msg = store.state.loginStatus
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/vuex/son.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="son">
+    <button @click="login">登录</button>
+    <button @click="out">退出</button>
+  </div>
+</template>
+
+<script>
+import store from '../../store'
+export default {
+    methods: {
+        login() {
+            store.commit('changeStatus', '已登录')
+        },
+        out() {
+            store.commit('changeStatus', '退出登录啦')
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
