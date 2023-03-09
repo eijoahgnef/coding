4b37b37cd74ef94cddc2ee359724bddf0b3881ad
冯浩洁 <1686913847@qq.com>
2023-03-09 20:09:22 +0800

    EventBus

---
+++ vue2/组件通信/demo/src/components/eventBus/parent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="event">
+    {{ sonVal }}
+    <son></son>
+  </div>
+</template>
+
+<script>
+import son from './son.vue'
+import { eventBus } from './bus'
+export default {
+    components: {
+        son
+    },
+    data() {
+        return {
+            sonVal: ''
+        }
+    },
+    methods: {
+        gainMsg() {
+            eventBus.$on('msg', (msg) => {
+                this.sonVal = msg
+            })
+        },
+    },
+    mounted() {
+        this.gainMsg()
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/eventBus/son.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="son">
+    <input type="text" v-model="val">
+    <button @click="toParent">发送给父组件</button>
+  </div>
+</template>
+
+<script>
+import { eventBus } from './bus'
+export default {
+    data() {
+        return {
+            val: ''
+        }
+    },
+    methods: {
+        toParent() {
+            console.log(this.val);
+            console.log(eventBus);
+            eventBus.$emit('msg', this.val)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
