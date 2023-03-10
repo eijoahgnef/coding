be1e8f430ad98141f1566b9538373a875d46ed6b
冯浩洁 <1686913847@qq.com>
2023-03-10 12:52:28 +0800

    slot

---
+++ vue2/组件通信/demo/src/components/slot/parent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="slot">
+    <son v-slot="slotProps">
+        <ul>
+            <li v-for="(item, index) in slotProps.list" :key="index">{{ item }}</li>
+        </ul>
+    </son>
+  </div>
+</template>
+
+<script>
+import son from './son.vue'
+export default {
+    components: {
+        son
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/slot/son.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="son">
+    <input type="text" v-model="msg"><button @click="give">传输</button>
+    <slot :list="list"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: [],
+            msg: ""
+        }
+    },
+    methods: {
+        give() {
+            this.list.push(this.msg)
+            this.msg = ""
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
