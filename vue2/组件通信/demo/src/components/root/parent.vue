3916c147aeb24a1b2e5d5e0a94b864d08bc3bd39
冯浩洁 <1686913847@qq.com>
2023-03-10 11:17:29 +0800

    $root

---
+++ vue2/组件通信/demo/src/components/root/parent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+    <span>{{ gainVal }}</span>
+    <button @click="gain">获取</button>
+    <son></son>
+</div>
+  
+</template>
+
+<script>
+import son from './son.vue'
+export default {
+    components: {
+        son
+    },
+    data() {
+        return {
+            gainVal: '父组件的值'
+        }
+    },
+    methods: {
+        gain() {
+            this.gainVal = this.$root.rootVal
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/组件通信/demo/src/components/root/son.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="son">
+    son
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
