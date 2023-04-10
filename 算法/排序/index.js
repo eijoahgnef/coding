e83b90ede6df77b6ddc400edfb32a03551492e88
冯浩洁 <1686913847@qq.com>
2023-04-10 23:32:09 +0800

    排序

--- 算法/排序/index.js
+++ 算法/排序/index.js
@@ -25,5 +25,50 @@ function selectSort(arr) {
     }
     return arr
 }
-const arr = [2,35,15,123,51,32,32,12,124]
-console.log(selectSort(arr));
+
+// 快速排序，选一个值为基准值，然后大的放右边，小的放左边，一样大的放中间
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr
+    } else {
+        let pivot = arr[0]
+        let pivotArr = []
+        let lowArr = []
+        let highArr = []
+        arr.forEach(item => {
+            if (item > pivot) {
+                highArr.push(item)
+            } else if (item < pivot) {
+                lowArr.push(item)
+            } else {
+                pivotArr.push(item)
+            }
+        })
+        return quickSort(lowArr).concat(pivotArr).concat(quickSort(highArr))
+    }
+}
+
+//  归并排序，排序一个数组，我们先把数组从中间分成前后两部分，然后对前后两部分分别排序，再将排好序的两部分合并在一起，这样整个数组就都有序了 
+function mergeSort(arr) {
+    const len = arr.length
+    if (len < 2) return arr
+    let middle = Math.floor(len/2)
+    let left = arr.slice(0, middle)
+    let right = arr.slice(middle)
+    return merge(mergeSort(left), mergeSort(right))
+}
+function merge(left, right) {
+	const result = [];
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+	while (left.length) result.push(left.shift());
+	while (right.length) result.push(right.shift());
+	return result;
+};
+const arr = [2,35,15,123,51,123,32,32,12,124]
+console.log(mergeSort(arr));
